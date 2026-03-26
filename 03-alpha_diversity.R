cdbb2374e9d4723bb4378dc10fab24bd837c02cc
Emily Schmeltzer <emilyrschmeltzer@gmail.com>
2026-03-26 16:57:35 -0400

    Redo shapiro test, remove forward_dada2 file and f&r file

--- 03-alpha_diversity.R
+++ 03-alpha_diversity.R
@@ -8,6 +8,8 @@ library(dplyr)
 library(rstatix)
 library(broom)
 library(tibble)
+library(lme4)
+library(emmeans)
 
 #set seed for reproducibility
 set.seed(123)
@@ -29,6 +31,25 @@ alphadiv <- estimate_richness(ticks, measures = c("Shannon")) %>%
 
 #Look at distribution and test for normality
 hist(alphadiv$Shannon) #normal-ish
-shapiro.test(alphadiv$Shannon) #is normal W = 0.98223, p = 0.07
+shapiro.test(alphadiv$Shannon) #is normal W = 0.98223, p = 0.07, will move forward with linear model
+
+# linear model shannon diversity
+mod.shannon <- lm(Shannon ~ Habitat*Treatment + Season + Lifestage, data=alphadiv)
+shapiro.test(residuals(mod.shannon)) # residuals normal
+
+lm_shan <- broom::tidy(anova(mod.shannon)) # Habitat and Treatment significant 
+lm_shan$Metric <- "Shannon"
+
+# pairwise comparisons
+mod_shannon_pw <- emmeans(mod.shannon, pairwise ~ Treatment, adjust = "tukey") 
+mod_shannon_pw <- broom::tidy(mod_shannon_pw$contrasts)
+mod_shannon_pw <- rstatix::add_significance(data = as.data.frame(mod_shannon_pw), p.col = "adj.p.value")
+mod_shannon_pw$Metric <- "Shannon"
+
+# pairwise comparisons
+mod_shannon_pw2 <- emmeans(mod.shannon, pairwise ~ Treatment|Habitat, adjust = "tukey") 
+mod_shannon_pw2 <- broom::tidy(mod_shannon_pw2$contrasts)
+mod_shannon_pw2 <- rstatix::add_significance(data = as.data.frame(mod_shannon_pw2), p.col = "adj.p.value")
+mod_shannon_pw2$Metric <- "Shannon"
 
 
