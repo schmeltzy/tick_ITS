71b1b27cebf5b3c1de026b20fb06d27b57707899
Emily Schmeltzer <emilyrschmeltzer@gmail.com>
2026-03-31 09:44:33 -0400

    Clean up, change pairwise model 2

--- 03-alpha_diversity.R
+++ 03-alpha_diversity.R
@@ -8,7 +8,6 @@ library(dplyr)
 library(rstatix)
 library(broom)
 library(tibble)
-library(lme4)
 library(emmeans)
 
 #set seed for reproducibility
@@ -19,10 +18,12 @@ ps.rare <- readRDS(here::here("output/ps.rare.rds"))
 
 #visualize alpha diversity just to make sure everything looks normal
 plot_richness(ps.rare, x="Treatment", measures=c("Shannon", "Chao1"), color="Lifestage")
-
+#+facet_grid(~Season)
 
 ####let's subset to just look at ticks (not soil)
 ticks <- subset_samples(ps.rare, Type=="BLT")
+plot_richness(ticks, x="Lifestage", measures=c("Shannon"), color="Treatment") +
+  facet_grid(~Habitat+Season)
 
 #calculate shannon diversity (richness and evenness)
 alphadiv <- estimate_richness(ticks, measures = c("Shannon")) %>%
@@ -33,12 +34,16 @@ alphadiv <- estimate_richness(ticks, measures = c("Shannon")) %>%
 hist(alphadiv$Shannon) #normal-ish
 shapiro.test(alphadiv$Shannon) #is normal W = 0.98223, p = 0.07, will move forward with linear model
 
+
 # linear model shannon diversity
-mod.shannon <- lm(Shannon ~ Habitat*Treatment + Season + Lifestage, data=alphadiv)
+mod.shannon <- glm(Shannon ~ Habitat + Treatment*Lifestage + Season, data=alphadiv)
 shapiro.test(residuals(mod.shannon)) # residuals normal
 
-lm_shan <- broom::tidy(anova(mod.shannon)) # Habitat and Treatment significant 
-lm_shan$Metric <- "Shannon"
+glm_shan <- broom::tidy(anova(mod.shannon)) # Habitat and Treatment significant 
+glm_shan$Metric <- "Shannon"
+
+#check out means across lifestages
+emmip(mod.shannon, Habitat ~ Treatment | Lifestage)
 
 # pairwise comparisons
 mod_shannon_pw <- emmeans(mod.shannon, pairwise ~ Treatment, adjust = "tukey") 
@@ -46,10 +51,16 @@ mod_shannon_pw <- broom::tidy(mod_shannon_pw$contrasts)
 mod_shannon_pw <- rstatix::add_significance(data = as.data.frame(mod_shannon_pw), p.col = "adj.p.value")
 mod_shannon_pw$Metric <- "Shannon"
 
-# pairwise comparisons
-mod_shannon_pw2 <- emmeans(mod.shannon, pairwise ~ Treatment|Habitat, adjust = "tukey") 
+mod_shannon_pw2 <- emmeans(mod.shannon, pairwise ~ Treatment | Lifestage, adjust = "tukey") 
 mod_shannon_pw2 <- broom::tidy(mod_shannon_pw2$contrasts)
 mod_shannon_pw2 <- rstatix::add_significance(data = as.data.frame(mod_shannon_pw2), p.col = "adj.p.value")
 mod_shannon_pw2$Metric <- "Shannon"
+
+# save
+write.csv(glm_shan, here::here("output/glm_alpha_shannon.csv"))
+write.csv(mod_shannon_pw, here::here("output/pairwise_alpha_treatment.csv"))
+write.csv(mod_shannon_pw2, here::here("output/pairwise_alpha_lifestage.csv"))
+
+
 
 
