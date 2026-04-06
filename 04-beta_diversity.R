f6feb5d39619b569dc485f5ebc7b53b1cfe1af4b
Emily Schmeltzer <emilyrschmeltzer@gmail.com>
2026-04-06 17:01:16 -0400

    Redoing distance calculation.

--- 04-beta_diversity.R
+++ 04-beta_diversity.R
@@ -3,9 +3,9 @@
 #beta diversity
 
 library(phyloseq)
-library(dada2)
+library(microViz)
+library(dplyr)
 library(ggplot2)
-library(microbiome)
 library(vegan)
 
 #set seed
@@ -14,30 +14,39 @@ set.seed(123)
 #load in unrarefied phyloseq object
 ps <- readRDS("phyloseq_obj.rds")
 
-#will be using Aitchinson distance to asses beta, so need to transform
-#using Euclidean distances
+# does habitat, treatment, lifestage, season, or sex affect beta diversity?
+#use aitchison distance for beta (clr transform followed by euclidean distance matrix)
+aitch <- dist_calc(ps, dist = "aitchison")
+aitch.dist <- dist_get(aitch)
 
-#clr transform
-clr <- microbiome::transform(ps, 'clr')
-
-#relative abundance transform
-ps_rel <- filter_taxa(ps, function(x) mean(x) > 0.1, TRUE)
-ps_rel <- transform_sample_counts(ps_rel, function(x) x / sum(x) )
-ps_rel
-#1523 taxa and 140 samples
-
-#filter taxa
-ps_rel_filt <- filter_taxa(ps_rel, function(x) mean(x) > 1e-5, TRUE)
-#1503 taxa
-
-rel_abun <- ps_rel@otu_table
-rel_abun <- as.matrix(rel_abun)
-write.table(rel_abun, file = "rel_abun_new.txt", sep = "\t")
+#get sample data from the matrix just to make it easier
+aitch.sampledf <- data.frame(sample_data(aitch))
 
 
-# save ps objects
-
-save(ps_rel, file = "ps_rel.RData") #rel abundance
-save(clr, file = "ps_clr.RData") #clr transformed
+# #will be using Aitchinson distance to asses beta, so need to transform
+# #using Euclidean distances
+# 
+# #clr transform
+# clr <- microbiome::transform(ps, 'clr')
+# 
+# #relative abundance transform
+# ps_rel <- filter_taxa(ps, function(x) mean(x) > 0.1, TRUE)
+# ps_rel <- transform_sample_counts(ps_rel, function(x) x / sum(x) )
+# ps_rel
+# 
+# 
+# #filter taxa
+# ps_rel_filt <- filter_taxa(ps_rel, function(x) mean(x) > 1e-5, TRUE)
+# 
+# 
+# rel_abun <- ps_rel@otu_table
+# rel_abun <- as.matrix(rel_abun)
+# write.table(rel_abun, file = "rel_abun_new.txt", sep = "\t")
+# 
+# 
+# # save ps objects
+# 
+# save(ps_rel, file = "ps_rel.RData") #rel abundance
+# save(clr, file = "ps_clr.RData") #clr transformed
 
 
