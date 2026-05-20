c03e2877a8bfcfdbc813e81fa936e0c184db6a24
Emily Schmeltzer <emilyrschmeltzer@gmail.com>
2026-05-20 08:35:03 -0400

    Added figures by habitat

--- 02-relative_abundance.R
+++ 02-relative_abundance.R
@@ -6,6 +6,7 @@ library(phyloseq)
 library(here)
 library(fantaxtic)
 library(ggpubr)
+library(ggpubfigs)
 library(dplyr)
 library(rstatix)
 library(broom)
@@ -98,15 +99,12 @@ ps_trans_genus_melt$genus[ps_trans_genus_melt$genus != "Alternaria" &
 colors <- c(friendly_pal("glasbey_twelve"),"lightgrey","maroon")
 #reorder colors to match genera of other plots for consistency
 colors <- c("#9A4D42","#FF0000","#000033","#00FF00","maroon","#009FFF","#FFD300","#005300","#0000FF","orange","#00FFBE","#FF00B6","#1F9698","lightgrey")
-#also reorder lifestage levels
-ps_trans_genus_melt$Lifestage <- factor(ps_trans_genus_melt$Lifestage, levels = c("larva", "nymph","adult"))
-
 
 plot_genus = ggplot(ps_trans_genus_melt, aes(x = Treatment, y=Abundance)) + 
   geom_bar(stat="identity", position="fill", aes(fill = reorder(genus, Abundance))) +
   scale_fill_manual(values= colors, 
                    na.value = "transparent")  +
-  facet_grid(~Habitat+Lifestage, scales = "free_x", space = "free_x") +
+  facet_grid(~Habitat, scales = "free_x", space = "free_x") +
   theme_bw(base_line_size = 1, base_rect_size = 1.5) +
   theme(axis.text = element_text(face = "bold", size = 14), 
         axis.title = element_text(face = "bold", size = 14), 
@@ -127,51 +125,190 @@ ggplot2::ggsave(here::here("output/plot_rel_abund.png"), plot_genus,
 
 #================
 
-#what if we do the same thing but separated by habitat and look at at least 1% abundance
+#what if we do the same thing but separated by habitat and look at top 30 most abundant
 forest <- subset_samples(ps.trans, Habitat=="forested")
 open <- subset_samples(ps.trans, Habitat=="open")
 
-##prune to get most abundant taxa 
-top30.forest <- names(sort(taxa_sums(forest), decreasing = TRUE))[1:30] 
-ps.top30.forest <- prune_taxa(top30.forest, forest)
+# ##prune to get most abundant taxa 
+# top30.forest <- names(sort(taxa_sums(forest), decreasing = TRUE))[1:30] 
+# ps.top30.forest <- prune_taxa(top30.forest, forest)
+# 
+# tol12extras <- c("#332288", "#6699CC", "#88CCEE", "#44AA99", "#117733", "#999933", 
+#            "#DDCC77", "#661100", "#CC6677", "#AA4499", "#882255", "#CCBB44","black","blue","magenta","maroon","orange","yellow","purple","pink","darkblue","lightblue")
+# 
+# ## Plot relative abundance ====
+# ps.top30.forest <- tax_glom(ps.top30.forest, taxrank = "Order", NArm = FALSE)
+# #put samples in order
+# ps.top30.forest@sam_data$Lifestage <- factor(ps.top30.forest@sam_data$Lifestage, levels = c("larva", "nymph","adult"))
+# 
+# 
+# plot.top30.forest <- plot_bar(ps.top30.forest, x="Abundance", y="Sample", fill="Order") +
+#   facet_wrap(~Treatment, scales = "free_y") + 
+#   scale_fill_manual(values = tol12extras) 
+# plot.top30.forest <- plot.top30.forest + theme_bw(base_line_size = 1, base_rect_size = 1.5) + 
+#   theme(axis.text.x = element_text(face = "bold", size = 11.5), title = element_text(face = "bold"), 
+#         axis.text.y = element_blank()) +
+#   xlab("Relative Abundance (%)") + ylab("Sample") +
+#   labs(title = "Top 30 most abundant tick-associated fungi") + theme(strip.text = element_text(face = "bold", size = 11)) +
+#   labs(fill = "Order") 
+# plot.top30.forest
 
-tol12extras <- c("#332288", "#6699CC", "#88CCEE", "#44AA99", "#117733", "#999933", 
-           "#DDCC77", "#661100", "#CC6677", "#AA4499", "#882255", "#CCBB44","black","blue","magenta","maroon","orange","yellow","purple","pink")
+###maybe 1% abundance is better
+#melt at genus level
+ps_trans_forest_melt <- forest %>%
+  tax_glom(taxrank = "Genus") %>%
+  psmelt()
 
-## Plot relative abundance ====
-ps.top30.forest <- tax_glom(ps.top30.forest, taxrank = "Family", NArm = FALSE)
+#get genera with mean relative abundance >1% across all samples
+forest_sum <- ps_trans_forest_melt %>% group_by(Genus) %>% dplyr::summarise(Average = mean(Abundance), sd = sd(Abundance), min = min(Abundance), max = max(Abundance))
+forest_sub <- forest_sum[which(forest_sum$Average > 1),]
+forest_names <- forest_sub$Genus
+forest_names
 
-plot.top30.forest <- plot_bar(ps.top30.forest, x="Sample", fill="Family") +
-  #facet_grid(vars(Lifestage), scales = "free_x") + 
-  scale_fill_manual(values = tol12extras) 
-plot.top30.forest <- plot.top30.forest + theme_bw(base_line_size = 1, base_rect_size = 1.5) + 
-  theme(axis.text.x = element_text(size = 0, angle = 90), 
-        axis.text.y = element_text(face = "bold", size = 11.5), title = element_text(face = "bold")) + xlab("Sample") + ylab("Relative Abundance (%)") +
-  labs(title = "Top 30 most abundant tick-associated fungi") + theme(strip.text = element_text(face = "bold", size = 11)) +
-  labs(fill = "Family") 
-plot.top30.forest
+write.csv(forest_sub, here::here("output/top_forest_relabund_genera.csv"))
+
+#replace genus with <1% abundance with NAs
+ps_trans_forest_melt$genus <- ps_trans_forest_melt$Genus
+
+ps_trans_forest_melt$genus[ps_trans_forest_melt$genus != "Cladosporium" & 
+                            ps_trans_forest_melt$genus != "Fusarium" &
+                            ps_trans_forest_melt$genus != "Papiliotrema" &
+                            ps_trans_forest_melt$genus != "Plectosphaerella" &
+                            ps_trans_forest_melt$genus != "Unclassified Basidiomycota" &
+                            ps_trans_forest_melt$genus != "Unclassified Fungi" &
+                            ps_trans_forest_melt$genus != "Unclassified Mycosphaerellaceae" &
+                            ps_trans_forest_melt$genus != "Curvibasidium" &
+                            ps_trans_forest_melt$genus != "Mycosphaerella" &
+                            ps_trans_forest_melt$genus != "Ramularia" &
+                            ps_trans_forest_melt$genus != "Unclassified Helotiales" &
+                            ps_trans_forest_melt$genus != "Pilidium" &
+                            ps_trans_forest_melt$genus != "Unclassified Amphisphaeriales" &
+                            ps_trans_forest_melt$genus != "Unclassified Didymellaceae" &
+                            ps_trans_forest_melt$genus != "Unclassified Leotiomycetes" & 
+                            ps_trans_forest_melt$genus != "Vishniacozyma"  ] <- NA
+
+#make our plot
+colors <- c(friendly_pal("glasbey_twelve"),"lightgrey","maroon")
+#reorder colors to match genera of other plots for consistency
+colors <- c("#FF00B6","lightgrey","#00FFBE","orange","#1F9698","#FFD300","#009FFF","#00FF00","#005300","#000033","#9A4D42","maroon","#FF0000","#FF9DA7","#440154","#0000FF")
+#ugh these are reversed!
+colors <- rev(c("#FF00B6","lightgrey","#00FFBE","orange","#1F9698","#FFD300","#009FFF","#00FF00","#005300","#000033","#9A4D42","maroon","#FF0000","#FF9DA7","#440154","#0000FF"))
+#also reorder lifestage levels
+ps_trans_forest_melt$Lifestage <- factor(ps_trans_forest_melt$Lifestage, levels = c("larva", "nymph","adult"))
+#and season levels so they group together nicely
+ps_trans_forest_melt$Season <- factor(ps_trans_forest_melt$Season, levels = c("spring", "fall"))
+
+plot_forest = ggplot(ps_trans_forest_melt, aes(x = Lifestage, y=Abundance)) + 
+  geom_bar(stat="identity", position="fill", aes(fill = reorder(genus, Abundance))) +
+  scale_fill_manual(values= colors, 
+                    na.value = "transparent")  +
+  facet_grid(~Treatment, scales = "free_x", space = "free_x") +
+  theme_bw(base_line_size = 1, base_rect_size = 1.5) +
+  theme(axis.text = element_text(face = "bold", size = 14), 
+        axis.title = element_text(face = "bold", size = 14), 
+        title = element_text(face = "bold")) +
+  ylab("Relative Abundance (%)") +
+  xlab("Life Stage") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 11)) +
+  theme(strip.text = element_text(face = "bold", size = 11)) +
+  theme(legend.position = "right", legend.text = element_text(face = "bold", size = 10))+
+  labs(fill = "Genus")
+plot_forest
+
+#save fig
+ggplot2::ggsave(here::here("output/plot_forest_relabund.png"), plot_forest,
+                height = 500, width = 500, units = "mm",
+                scale = 0.5, dpi = 1000)
+
+#==============
+#now with open data
+#melt at genus level
+ps_trans_open_melt <- open %>%
+  tax_glom(taxrank = "Genus") %>%
+  psmelt()
+
+#get genera with mean relative abundance >1% across all samples
+open_sum <- ps_trans_open_melt %>% group_by(Genus) %>% dplyr::summarise(Average = mean(Abundance), sd = sd(Abundance), min = min(Abundance), max = max(Abundance))
+open_sub <- open_sum[which(open_sum$Average > 1),]
+open_names <- open_sub$Genus
+open_names
+
+write.csv(open_sub, here::here("output/top_open_relabund_genera.csv"))
+
+#replace genus with <1% abundance with NAs
+ps_trans_open_melt$genus <- ps_trans_open_melt$Genus
+
+ps_trans_open_melt$genus[ps_trans_open_melt$genus != "Cladosporium" & 
+                             ps_trans_open_melt$genus != "Alternaria" &
+                             ps_trans_open_melt$genus != "Papiliotrema" &
+                             ps_trans_open_melt$genus != "Dioszegia" &
+                             ps_trans_open_melt$genus != "Hannaella" &
+                             ps_trans_open_melt$genus != "Unclassified Fungi" &
+                             ps_trans_open_melt$genus != "Mrakia" &
+                             ps_trans_open_melt$genus != "Paraphaeosphaeria" &
+                             ps_trans_open_melt$genus != "Parastagonospora" &
+                             ps_trans_open_melt$genus != "Septoria" &
+                             ps_trans_open_melt$genus != "Stagonosporopsis" &
+                             ps_trans_open_melt$genus != "Tricellula" &
+                             ps_trans_open_melt$genus != "Unclassified Didymellaceae" &
+                             ps_trans_open_melt$genus != "Vishniacozyma"  ] <- NA
+
+#make our plot
+colors <- c(friendly_pal("glasbey_twelve"),"lightgrey","maroon")
+#reorder colors to match genera of other plots for consistency
+colors <- c("lightgrey","#1F9698","#0000FF","orange","#FF0000","#005300","#FF9DA7","maroon","#440154","#9A4D42","#00FF00","#000033","#009FFF","#FFD300")
+#reverse the order
+colors <- rev(c("lightgrey","#1F9698","#0000FF","orange","#FF0000","#005300","#FF9DA7","maroon","#440154","#9A4D42","#00FF00","#000033","#009FFF","#FFD300"))
+
+ps_trans_open_melt$Season <- factor(ps_trans_open_melt$Season, levels = c("spring", "fall"))
+
+plot_open = ggplot(ps_trans_open_melt, aes(x = Season, y=Abundance)) + 
+  geom_bar(stat="identity", position="fill", aes(fill = reorder(genus, Abundance))) +
+  scale_fill_manual(values= colors, 
+                    na.value = "transparent")  +
+  facet_grid(~Treatment, scales = "free_x", space = "free_x") +
+  theme_bw(base_line_size = 1, base_rect_size = 1.5) +
+  theme(axis.text = element_text(face = "bold", size = 14), 
+        axis.title = element_text(face = "bold", size = 14), 
+        title = element_text(face = "bold")) +
+  ylab("Relative Abundance (%)") +
+  xlab("Season") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 11)) +
+  theme(strip.text = element_text(face = "bold", size = 11)) +
+  theme(legend.position = "right", legend.text = element_text(face = "bold", size = 10))+
+  labs(fill = "Genus")
+plot_open
+
+#save fig
+ggplot2::ggsave(here::here("output/plot_open_relabund.png"), plot_open,
+                height = 500, width = 500, units = "mm",
+                scale = 0.5, dpi = 1000)
 
 
 #===============================
 
 ## Let's try to just look at some of the entomopathogens
 ## Subset into the 4 we know about (mostly)
-ps.gen <- tax_glom(ps.trans, taxrank = "Genus", NArm = FALSE)
-ps.pathogens <- subset_taxa(ps.gen, Genus == "Metarhizium" | 
-              Genus == "Beauveria" | 
-              Genus == "Lecanicillium" |
-              Genus == "Cordyceps" |
-              Genus == "Paecilomyces" )
+ps.pathogens <- subset_taxa(ps.trans, Genus == "Metarhizium" | 
+                              Genus == "Beauveria" | 
+                              Genus == "Lecanicillium" |
+                              Genus == "Cordyceps" |
+                              Genus == "Paecilomyces" )
 
 #trim samples to just get the ones with entos
 path.pruned <- prune_samples(sample_sums(ps.pathogens@otu_table) > 0, ps.pathogens)
 
 #put treatments in order for the plot
 path.pruned@sam_data$Treatment <- factor(path.pruned@sam_data$Treatment, levels = c("unmowed", "mowed","unmanaged"))
+#add column for taxon ID with genus+species info
+tax_df <- as.data.frame(tax_table(path.pruned))
 
+tax_df$Species <- paste(tax_df$Genus, tax_df$Species, sep = " ")
+
+tax_table(path.pruned) <- tax_table(as.matrix(tax_df))
 
 ## entomopathogen relative abundance figure
-plot.entos <- plot_bar(path.pruned, x="Lifestage", fill = "Genus") +
+plot.entos <- plot_bar(path.pruned, x="Lifestage", fill = "Species") +
   facet_wrap(~Treatment) +
   geom_bar(stat="identity")+
   scale_fill_manual(values = sample_colors)
